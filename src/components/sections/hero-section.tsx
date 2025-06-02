84976b7749761f9a116d3f42071eb595e4aee9aa
kcshikhar906 <shikharkc63@gmail.com>
2025-06-02 14:36:31 +1000

    'image again'

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -142,7 +142,7 @@ export default function HeroSection({ id }: HeroSectionProps) {
           {/* <div className="md:w-2/5 lg:w-1/2 flex justify-center md:justify-end order-1 md:order-2 animate-in fade-in slide-in-from-right-8 duration-700 delay-200">
             <div className="relative w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:w-[400px] lg:h-[400px] rounded-full overflow-hidden shadow-2xl border-4 border-primary/30">
               <Image
-                src="/photo.jpg"
+                src="/images/hero-profile.jpg"
                 alt="Shikhar KC Profile"
                 layout="fill"
                 objectFit="cover"
@@ -154,7 +154,7 @@ export default function HeroSection({ id }: HeroSectionProps) {
           <div className="md:w-2/5 lg:w-1/2 flex justify-center md:justify-end order-1 md:order-2 animate-in fade-in slide-in-from-right-8 duration-700 delay-200">
             <div className="relative w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:w-[400px] lg:h-[400px] rounded-full overflow-hidden shadow-2xl border-4 border-primary/30">
               <Image
-                src="/photo.jpg"
+                src="/images/hero-profile.jpg"
                 alt="Shikhar KC Profile"
                 width={400}
                 height={400}
